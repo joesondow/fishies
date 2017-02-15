b0213615502ca49fd3d8beab7d276df98af861d6
Joe Sondow <joesondow@users.noreply.github.com>
2017-02-15 01:44:47 -0800

    Support for oauth tokens in environment variables without dots in env var keys, to satisfy a restriction of AWS Lambda.

--- src/main/java/sondow/fishies/LambdaRequestHandler.java
+++ src/main/java/sondow/fishies/LambdaRequestHandler.java
@@ -6,18 +6,48 @@ package sondow.fishies;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
 /**
  * @author joesondow
  *
  */
 public class LambdaRequestHandler implements RequestHandler<Object, Object> {
 
-	/* (non-Javadoc)
-	 * @see com.amazonaws.services.lambda.runtime.RequestHandler#handleRequest(java.lang.Object, com.amazonaws.services.lambda.runtime.Context)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.amazonaws.services.lambda.runtime.RequestHandler#handleRequest(java.
+	 * lang.Object, com.amazonaws.services.lambda.runtime.Context)
 	 */
 	public Object handleRequest(Object input, Context context) {
 		AquariumBuilder builder = new AquariumBuilder();
-		Tweeter tweeter = new Tweeter();
+		
+		// AWS Lambda only allows underscores in environment variables, not dots, so the default ways twitter4j finds keys aren't
+		// possible. Instead, write your own code that gets the configuration either from Lambda-friendly environment variables
+		// or from a default twitter4j.properties file at the project root, or on the classpath, or in WEB-INF.
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		String consumerKeyEnvVar = System.getenv("twitter4j_oauth_consumerKey");
+		String consumerSecretEnvVar = System.getenv("twitter4j_oauth_consumerSecret");
+		String accessTokenEnvVar = System.getenv("twitter4j_oauth_accessToken");
+		String accessTokenSecretEnvVar = System.getenv("twitter4j_oauth_accessTokenSecret");
+		if (consumerKeyEnvVar != null) {
+			cb.setOAuthConsumerKey(consumerKeyEnvVar);
+		}
+		if (consumerSecretEnvVar != null) {
+			cb.setOAuthConsumerSecret(consumerSecretEnvVar);
+		}
+		if (accessTokenEnvVar != null) {
+			cb.setOAuthAccessToken(accessTokenEnvVar);
+		}
+		if (accessTokenSecretEnvVar != null) {
+			cb.setOAuthAccessTokenSecret(accessTokenSecretEnvVar);
+		}
+		Configuration config = cb.build();
+		
+		Tweeter tweeter = new Tweeter(config);
 		return tweeter.tweet(builder.build());
 	}
 
--- src/main/java/sondow/fishies/Tweeter.java
+++ src/main/java/sondow/fishies/Tweeter.java
@@ -4,11 +4,23 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class Tweeter {
+	
+	private Configuration config;
+
+	public Tweeter() {
+		this(new ConfigurationBuilder().build());
+	}
+	
+	public Tweeter(Configuration config) {
+		this.config = config;
+	}
 
 	public Status tweet(String message) {
-		Twitter twitter = TwitterFactory.getSingleton();
+		Twitter twitter = new TwitterFactory(config).getInstance();
 		try {
 			Status status = twitter.updateStatus(message);
 			System.out.println("Successfully tweeted message: " + message + " with status " + status);
