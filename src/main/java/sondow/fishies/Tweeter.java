f748f52e8073e21fe8cddecc81bea2e9b860cec8
Joe Sondow <joesondow@users.noreply.github.com>
2017-02-15 00:50:29 -0800

    Implemented tweeting.

--- src/main/java/sondow/fishies/Tweeter.java
+++ src/main/java/sondow/fishies/Tweeter.java
@@ -1,8 +1,19 @@
 package sondow.fishies;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 public class Tweeter {
 
 	public void tweet(String message) {
-		
+		Twitter twitter = TwitterFactory.getSingleton();
+		try {
+			Status status = twitter.updateStatus(message);
+			System.out.println("Successfully tweeted message: " + message + " with status " + status);
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
 	}
 }
